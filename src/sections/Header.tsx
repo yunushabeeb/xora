e3a5bca48a26e7a86fbb16317bd6d5c95b513bee
Habeeb Yunus Habeeb <habeebdh1@outlook.com>
2024-10-04 04:24:45 +0100

    Add scroll tracker

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,20 +1,44 @@
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { LinkProps, Link as LinkScroll } from 'react-scroll';
-
-const NavLink = ({ title, to }: LinkProps) => (
-  <LinkScroll
-    to={to}
-    className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
-  >
-    {title}
-  </LinkScroll>
-);
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title, to }: LinkProps) => (
+    <LinkScroll
+      to={to}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+      onClick={() => setIsOpen(false)}
+    >
+      {title}
+    </LinkScroll>
+  );
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4">
+    <header
+      className={clsx(
+        'fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4',
+        hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]'
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a href="" className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
@@ -36,7 +60,7 @@ const Header = () => {
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
                     className="max-lg:hidden transition-transform duration-500 cursor-pointer"
